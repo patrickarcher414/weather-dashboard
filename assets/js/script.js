90f5e8798ce99f4632f3df522adfa84d524ada81
patrickarcher414 <patrickarcher414@gmail.com>
2022-04-04 15:45:28 -0500

    fixed fetch url

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,12 @@
 
 var lat, lon 
-const key = '3136490ddb596774fee3ee38098e9930'
+var key = '3136490ddb596774fee3ee38098e9930'
 var weatherUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${key}`
 var geoUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${key}`
-var city = 'milwaukee'
+var city = 'Milwaukee'
 
 // get city lat and lon with geocoding API
-fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${key}`)
+fetch("http://api.openweathermap.org/geo/1.0/direct?q=" + city +"&appid=" + key)
     .then(function(response) {
         return response.json()
     })
@@ -22,14 +22,14 @@ fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${key}`)
 
 
 // get weather data for lat and lon returned from geocoding fetch
-fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${key}`)
-    .then(function(response) {
-        return response.json()
-    })
-    .then(function(data) {
-        console.log(data)
+// fetch("https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&appid=" + key)
+//     .then(function(response) {
+//         return response.json()
+//     })
+//     .then(function(data) {
+//         console.log(data)
         
-    })
-    .catch(function(err) {
-        console.log(err)
-    })
+//     })
+//     .catch(function(err) {
+//         console.log(err)
+//     })
