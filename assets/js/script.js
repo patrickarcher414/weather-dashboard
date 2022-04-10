763cba25bac01850a10c3cf20cc0a72c91c353d8
patrickarcher414 <patrickarcher414@gmail.com>
2022-04-10 14:14:45 -0500

    city name saved to local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,31 +1,15 @@
+
+var cityInput = document.querySelector('#citySearch')
 var container = document.querySelector('#container')
 var key = '3136490ddb596774fee3ee38098e9930'
 var searchBtn = document.querySelector('#searchBtn')
 
-// var cityData = locationData[0]
 // var currentWeather = weatherData[0].current
 // var fivedayForecast = weatherData[0].daily
 
 // tomorrow's date
 //fivedayForecast[0].dt
 
-// // city name
-// cityData.name
-
-// // temp
-// currentWeather.temp
-
-// // feels like
-// currentWeather.feels_like
-
-// // wind
-// currentWeather.wind_speed
-
-// // humidity
-// currentWeather.humidity
-
-// // uv index
-// currentWeather.uvi
 
 function getLocationData(city) {
     fetch("http://api.openweathermap.org/geo/1.0/direct?q=" + city +"&appid=" + key)
@@ -33,9 +17,7 @@ function getLocationData(city) {
         return response.json()
     })
     .then(function(locationData) {
-        console.log(locationData)
         getCurrentWeather(locationData[0].lat, locationData[0].lon)
-       
     })
     .catch(function(err) {
         console.log(err)
@@ -48,11 +30,42 @@ function getCurrentWeather(lat, lon) {
         return response.json()
     })
     .then(function(weatherData) {
-        console.log(weatherData)
+        
         var currentDiv = document.createElement('div')
-        var cityName = document.createElement('h2')
+        container.appendChild(currentDiv)
 
-        container.innerText = weatherData.current.temp
+        var cityName = document.createElement('h2')
+        currentDiv.appendChild(cityName)
+        var city = localStorage.getItem("city")
+        cityName.innerText = city
+
+        var weatherList = document.createElement('ul')
+        currentDiv.appendChild(weatherList)
+
+        // create a list item with current temp
+        var tempEl = document.createElement('li')
+        tempEl.setAttribute('class', 'list-group list-group-flush')
+        tempEl.innerText = "Temp: " + Math.floor(weatherData.current.temp) + "°F"
+        weatherList.appendChild(tempEl)
+
+        // repeat for wind
+        var windEl = document.createElement('li')
+        windEl.setAttribute('class', 'list-group list-group-flush')
+        windEl.innerText = "Wind: " + Math.floor(weatherData.current.wind_speed) + " mph"
+        weatherList.appendChild(windEl)
+
+        // repeat for humidity
+        var humEl = document.createElement('li')
+        humEl.setAttribute('class', 'list-group list-group-flush')
+        humEl.innerText = "Humidity: " + weatherData.current.humidity + "%"
+        weatherList.appendChild(humEl)
+
+        //repeat for uv index
+        var uvEl = document.createElement('li')
+        uvEl.setAttribute('class', 'list-group list-group-flush')
+        uvEl.innerText = "UV Index: " + weatherData.current.uvi
+        weatherList.appendChild(uvEl)
+
     })
     .catch(function(err) {
         console.log(err)
@@ -61,7 +74,11 @@ function getCurrentWeather(lat, lon) {
 
 function handleSearch(ev) {
     ev.preventDefault();
-    getLocationData('milwaukee');
+    container.innerHTML = ""
+    var cityName = cityInput.value
+    localStorage.setItem("city", cityName)
+    console.log(cityName)
+    getLocationData(cityName);
     
 }
 searchBtn.addEventListener('click', handleSearch) 
